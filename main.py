64925041969c7bb5f9d2abf70ff1e2d9896cb580
LiterallyJohnny <liljohnnyrayy+github@gmail.com>
2021-01-28 11:24:24 -0600

    Asks if the user would like to play rps

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import random  # Allows randomizing things such as lists and strings
+
+# Asks the player if they would like to play rps. If yes, the program will continue onto the next loop.
+while True:
+    playRps = str(input('Would you like to play "Rock, Paper, Scissors"? (Y/n): '))
+
+    if playRps == "y":
+        print("Placeholder")
+
+    elif playRps == "n":
+        print("Terminating program...")
+        quit()
+
+    else:
+        print("Unknown input. Please enter a valid answer.")
+        continue
