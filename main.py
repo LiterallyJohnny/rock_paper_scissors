795537ef7443893a3778dc7f3a83e845a9a850a9
LiterallyJohnny <liljohnnyrayy+github@gmail.com>
2021-01-28 11:31:28 -0600

    Created a list of possible choices

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 
 import functions  # Imports the functions.py file
 
+possibleChoices = (
+    'r',
+    'p',
+    's',
+)
+
 # Asks the player if they would like to play rps. If yes, the program will continue onto the next loop.
 while True:
     playRps = str(input('Would you like to play "Rock, Paper, Scissors"? (Y/n): '))
