11d07bc374519fa6b3db39af43b347083eb6ae81
LiterallyJohnny <liljohnnyrayy+github@gmail.com>
2021-01-28 11:28:08 -0600

    Added the main function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random  # Allows randomizing things such as lists and strings
+import functions  # Imports the functions.py file
 
 # Asks the player if they would like to play rps. If yes, the program will continue onto the next loop.
 while True:
     playRps = str(input('Would you like to play "Rock, Paper, Scissors"? (Y/n): '))
 
     if playRps == "y":
-        print("Placeholder")
+        while True:
+            functions.playGame()
 
     elif playRps == "n":
         print("Terminating program...")
